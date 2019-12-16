b2e5f336bfe371168d6358c74e8ae7680b2053f2
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-12-15 23:47:16 -0800

    add simple example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import h337 from 'heatmapjs';
 
-function App() {
-  return <div>App</div>;
+class App extends React.Component {
+  heatmapRef = React.createRef();
+
+  componentDidMount() {
+    let heatmap = h337.create({
+      container: this.heatmapRef.current
+    });
+    heatmap.setData({
+      max: 5,
+      data: [
+        { x: 0, y: 0, value: 1 },
+        { x: 100, y: 100, value: 2 },
+        { x: 200, y: 200, value: 3 },
+        { x: 300, y: 300, value: 4 },
+        { x: 400, y: 400, value: 5 }
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div ref={this.heatmapRef} style={{ width: 500, height: 500 }}></div>
+    );
+  }
 }
 
 export default App;
